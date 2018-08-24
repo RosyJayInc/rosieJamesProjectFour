ddaa254b6fc039607296847636c5d10241566b78
James MacDonald <jamesdavidmacdonald@gmail.com>
2018-08-24 15:07:36 -0400

    direction function + Database updates running

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -4,36 +4,161 @@ const app = {};
 app.apiKey = "Aps9Ru4I2VE16SVT-Uqa1m0_dnEV3AI15tq6yOCMbctU6mkJFtcs4CQiiet2bJvX";
 app.cityAndCountry = ", Toronto, Canada";
 app.map;
+app.pin;
 app.searchManager;
+app.directionsManager;
 
-app.determineResults =(results) =>{
+// Initialize Firebase
+app.config = {
+    apiKey: "AIzaSyDK_ozYtdxMbAEhZ6T3g79O5K-eHfCBKZw",
+    authDomain: "rosiejamesprojectfour.firebaseapp.com",
+    databaseURL: "https://rosiejamesprojectfour.firebaseio.com",
+    projectId: "rosiejamesprojectfour",
+    storageBucket: "rosiejamesprojectfour.appspot.com",
+    messagingSenderId: "360785100105"
+};
+firebase.initializeApp(app.config);
 
+
+app.dbRef = firebase.database().ref("project4SafeAreas");
+
+app.dbChanges = function(result = "empty"){
+    if(result != "empty"){
+        app.dbRef.on("value", function(snapshot){
+            let doesExist = false;
+            let safeAreas = snapshot.val();
+            // console.log(safeAreas);
+            // console.log(result.formattedAddress);
+            
+            
+            for(let area in safeAreas){
+                if(safeAreas[area].address === result.address.formattedAddress){
+                    doesExist = true;
+                }
+            }
+            if(doesExist === false){
+                const id = app.dbRef.push().key;
+                const itemReference = firebase.database().ref(`project4SafeAreas/${id}`);
+
+                itemReference.set({
+                    address: result.address.formattedAddress,
+                    safe: true,
+                    lat: result.location.latitude,
+                    long: result.location.longitude,
+                    key: id
+                });
+            }
+        });
+    }
+    else{
+        
+    }
+}
+app.determineResults = (addressData, results) =>{
+    
+    
     let resultString = "";
 
     if (results > 450) {
         resultString = $(`<p>${results} : Severe </p>`);
     }
     else if(results > 350){
+        resultString = $(`<p>${results} : Extremely high </p>`);
+        app.findSafeArea(addressData);
     }
     else if(results > 250){
         resultString = $(`<p>${results} : High </p>`);
     }
     else if(results > 150){
         resultString = $(`<p>${results} : Moderate</p>`);
+        app.dbChanges(addressData);
     }
     else if(results > 50){
         resultString = $(`<p>${results} : Low </p>`);
+        app.dbChanges(addressData);
     }
     else if(results >= 0 ){
         resultString = $(`<p>${results} : Negligible</p>`);
+        app.dbChanges(addressData);
     }
     else{
         resultString = $(`No results Found, Try Again`);
     }
     
     $(".textResults").append(resultString);
+    // console.log('getting results');
 }
 
+app.findSafeArea = function(unsafeAddress) {
+    console.log(app.dbRef);
+
+    let curLat = unsafeAddress.location.latitude;
+    let curLon = unsafeAddress.location.longitude;
+
+    console.log(curLat, curLon);
+    
+    let rangeVal = 0.02;
+
+    app.dbRef.once("value", function(snapshot){
+        let isNear = false;
+        let safeList = snapshot.val();
+        let closeAreas = [];
+
+        for (let area in safeList){
+            if (
+                ((curLat - rangeVal) < safeList[area].lat && safeList[area].lat < (curLat + rangeVal))
+                &&
+                ((curLon - rangeVal) < safeList[area].long && safeList[area].long < (curLon + rangeVal))
+            ){
+                isNear = true;
+                closeAreas.push(safeList[area]);
+            }
+        }
+        if(isNear === true){
+            console.log(closeAreas);
+            
+            let ranSpot = Math.floor(Math.random() * closeAreas.length);
+            console.log(closeAreas[ranSpot]);
+            $(".findSafe").on("click", function(){
+                app.getDirections(unsafeAddress, closeAreas[ranSpot]);
+            })
+            
+        }
+    });
+}
+app.getDirections = function(unsafe, safe){
+    console.log('test');
+    console.log(unsafe.address.formattedAddress, safe.address);
+    let unsafeString = unsafe.address.formattedAddress;
+    let safeString = safe.address;
+    let safeLat = safe.lat;
+    let safeLong = safe.long
+    
+    Microsoft.Maps.loadModule("Microsoft.Maps.Directions", function(){
+        //Create an instance of the directions manager.
+        app.directionsManager = new Microsoft.Maps.Directions.DirectionsManager(app.map);
+        
+        //Create waypoints to route between.
+        let currentPoint = new Microsoft.Maps.Directions.Waypoint({ 
+            address: unsafeString 
+        });
+        app.directionsManager.addWaypoint(currentPoint);
+        
+        
+        
+        let safePoint = new Microsoft.Maps.Directions.Waypoint({
+            address: safeString
+        });
+        app.directionsManager.addWaypoint(safePoint);
+
+        //Specify the element in which the itinerary will be rendered.
+        app.directionsManager.setRenderOptions({ itineraryContainer: '#directions' });
+        console.log(app.pin.getLocation());
+        app.map.entities.remove(app.pin);
+        //Calculate directions.
+        app.directionsManager.calculateDirections();
+    });
+}
 app.getMap = function(query) {
     let navigationBarMode = Microsoft.Maps.NavigationBarMode;
     app.map = new Microsoft.Maps.Map("#resultMap", {
@@ -63,7 +188,6 @@ app.geocodeQuery = function(query) {
             app.geocodeQuery(query);
         })
     } else {
-        console.log("test")
         let searchRequest = {
             where: query,
             callback: function(r) {
@@ -72,23 +196,19 @@ app.geocodeQuery = function(query) {
                     console.log(r.results);
                     let firstResult = r.results[0]
                     
-                    let pin = new Microsoft.Maps.Pushpin(firstResult.location,{
+                    app.pin = new Microsoft.Maps.Pushpin(firstResult.location,{
                         color: "red",
                         title: query
                     });
-
-                    let locationX = firstResult.location.longitude;
-                    let locationY = firstResult.location.latitude;
-
                     // make the database call here
-                    app.getCrimeData(locationX, locationY);
+                    app.getCrimeData(firstResult);
 
-                    app.map.entities.push(pin);
+                    app.map.entities.push(app.pin);
 
                     app.map.setView({bounds:firstResult.bestView});
                 }
             },
-            errorCallback: function(e) {
+            errorCallback: function() {
                 alert("no results found")
             }
         }
@@ -99,10 +219,13 @@ app.geocodeQuery = function(query) {
 } // geocode query ends
 
 
-app.getCrimeData = function(locationX, locationY) {
+app.getCrimeData = function(addressData) {
     console.log("getting crime data");
 
     const url = "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/Bicycle_Thefts/FeatureServer/0/query?";
+
+    let locationX = addressData.location.longitude;
+    let locationY = addressData.location.latitude;
 
     $.ajax({
         url: url,
@@ -121,9 +244,10 @@ app.getCrimeData = function(locationX, locationY) {
             where: "Occurrence_Year > 2016"
         }
     }).then((res)=>{
+        
         let results = res.features.length;
 
-        app.determineResults(results);
+        app.determineResults(addressData, results);
     });
 
 }
@@ -135,6 +259,8 @@ app.submitQuery = function() {
     $(".addressQuery").on("submit", function(e){
         e.preventDefault();
         let addressString = $(".queryText").val().trim();
+        //create node on firebase
+        
         app.getMap(addressString);
     });
 }
@@ -142,6 +268,9 @@ app.submitQuery = function() {
 
 app.init = function() {
     app.submitQuery();
+    // app.findSafeArea("test");
+    app.dbChanges();
+    
 }
 
 $(function(){
